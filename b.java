8d3e559267389c9449742668f282dc6f3c3f6b8d
Amit Kumar <amit.official323@gmail.com>
2022-07-20 12:34:12 +0530

    Master Branch Code Files

--- b.java
+++ b.java
@@ -1,6 +1,6 @@
 public class Main {
   public static void main(String[] args) {
-    System.out.println("A code file from the master Branch");
+    System.out.println("B code file from the Master branch");
   }
 }
 
